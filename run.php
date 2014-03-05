a27ed48d8e28e020087bb2bb71e433c306d21870
addshore <addshorewiki@gmail.com>
2014-03-05 13:32:31 +0100

    Add Wikidata/easyrdf_lite

--- run.php
+++ run.php
@@ -26,6 +26,9 @@ $hookTargets = array(
 		'puppet-composer',
 		'github-hook-updater',
 	),
+	'Wikidata' => array(
+		'easyrdf_lite',
+	),
 	'DataValues' => array(
 		'Geo',
 		'Number',
@@ -60,7 +63,7 @@ foreach( $hookTargets as $user => $repos ) {
 				'server' => 'chat.freenode.org',
 				'port' => '7000',
 				'room' => '#wikidata',
-				'nick' => 'github-' . $user,
+				'nick' => 'github-' . strtolower( $user ),
 				'ssl' => '1',
 			),
 			'events' => array(
